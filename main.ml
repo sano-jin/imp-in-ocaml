5d5dd297bc44b99f8e126e70f8b6850917cd4245
sano-jin <sano-jin@akane.waseda.jp>
2021-05-29 22:27:27 +0900

    updated

--- main.ml
+++ main.ml
@@ -8,7 +8,7 @@ let parse str =
   Parser.main Lexer.token 
     (Lexing.from_string str)
 
-(* "X := 4; Y := 1; Z := 0; while (Z < X) { Y := 2 * Y; Z := Z + 1 }" *)
+(* Main.run "X := 4; Y := 1; Z := 0; while (Z < X) { Y := 2 * Y; Z := Z + 1 }" -> [("X", 4); ("Y", 16); ("Z", 4)] *)
 let run str = 
   eval_command (parse str) []
 
